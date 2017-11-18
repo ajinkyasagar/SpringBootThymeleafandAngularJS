530742c52923cced5918c6e448919c5f63d3e47c
ajinkyasagar <sagarthakare1@gmail.com>
2017-11-18 15:14:01 +0530

    Commiting working code

---
+++ src/main/webapp/WEB-INF/js/application.js
@@ -0,0 +1,21 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.htm"
+    })
+    .when("/red", {
+        templateUrl : "red.htm"
+    })
+    .when("/green", {
+        templateUrl : "green.html"
+    })
+    .when("/blue", {
+        templateUrl : "blue.htm"
+    });
+});
+
+app.controller('myCtrl', function($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+});
